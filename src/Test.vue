f82f2954b3daf3eb1923358063cea4cec98186bb
fm <fm353488931@vip.qq.com>
2024-01-26 10:19:38 +0800

    init

---
+++ src/Test.vue
@@ -0,0 +1,18 @@
+<template>
+  <div v-for="(item, index) in items" :key="index" ref="colRef">colRef</div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+const colRef = ref([])
+
+const items = ref([])
+
+items.value = [{}, {}] 
+
+// setTimeout(() => {
+//   items.value = [{}, {}]
+// }, 300)
+
+console.log('1111') //change it and look console
+</script>
